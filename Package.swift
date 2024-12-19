85f5ce7c67199d8fe2517b01b64240740854c9f1
Marko Justinek <1733327+surpher@users.noreply.github.com>
2024-12-19 14:02:23 +1100

    Add initial package manifest and CHANGELOG

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "SwiftPactMockServer",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "PactSwiftMockServer",
+            targets: ["PactSwiftMockServer"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "PactSwiftMockServer",
+            url: "https://github.com/surpher/PactSwiftMockServerXCFramework/releases/download/v1.0.0/PactSwiftMockServer-v1.0.0.xcframework.zip",
+            checksum: ""
+        ),
+    ]
+)
