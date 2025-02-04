bcef71f1d21ad0cfa5b090e82a17157b1cf73186
Marko Justinek <1733327+surpher@users.noreply.github.com>
2025-02-04 15:20:59 +1100

    Release: v1.0.4

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PactSwiftMockServer",
-            url: "https://github.com/surpher/PactSwiftMockServerXCFramework/releases/download/v1.1.0/PactSwiftMockServer-v1.1.0.zip",
-            checksum: "c1952c331f6b1e7f1d61d6a2e6daf1a9d4c7cab9eab695df0cfd1a56f78e64ed"
+            url: "https://github.com/surpher/PactSwiftMockServerXCFramework/releases/download/v1.0.4/PactSwiftMockServer-v1.0.4.zip",
+            checksum: "e7e98f12b1fd47ee83ea7bdc5ae0d1274831cc250a59c6a5da68fbfad1f9cc25"
         ),
     ]
 )
